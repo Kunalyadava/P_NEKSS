9eecf3fb5db7490477b8d67d8db5880b03b9e441
Kunalyadava <kunalkr1857@gmail.com>
2025-02-12 02:24:37 +0530

    dateRangeAdded

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { TrackerComponent } from './tracker/tracker.component';
 import { DeptWiseOPDSummaryReportComponent } from './dept-wise-opdsummary-report/dept-wise-opdsummary-report.component';
+import { NameComponentComponent } from './name/name-component/name-component.component';
+import { TableComponent } from './name/table/table.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'tracker', pathMatch: 'full' },
     {path:"tracker",component:TrackerComponent} ,
-    {path:"summary",component:DeptWiseOPDSummaryReportComponent} 
+    {path:"summary",component:DeptWiseOPDSummaryReportComponent} ,
+    {path:"name",component:NameComponentComponent},
+    {path:"table",component:TableComponent}
 ];
