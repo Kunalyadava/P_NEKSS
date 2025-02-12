69b7d87b748b9bf5ff42b7d93381a27650acf4c4
Kunalyadava <kunalkr1857@gmail.com>
2025-02-13 02:39:55 +0530

    dateRangeAdded

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { TrackerComponent } from './tracker/tracker.component';
 import { DeptWiseOPDSummaryReportComponent } from './dept-wise-opdsummary-report/dept-wise-opdsummary-report.component';
 import { NameComponentComponent } from './name/name-component/name-component.component';
 import { TableComponent } from './name/table/table.component';
+import { DatesearchComponent } from './name/datesearch/datesearch.component';
+import { MonthsearchComponent } from './name/monthsearch/monthsearch.component';
+import { SelectwholeyearComponent } from './name/selectwholeyear/selectwholeyear.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'tracker', pathMatch: 'full' },
     {path:"tracker",component:TrackerComponent} ,
     {path:"summary",component:DeptWiseOPDSummaryReportComponent} ,
     {path:"name",component:NameComponentComponent},
-    {path:"table",component:TableComponent}
+    {path:"table",component:TableComponent},
+    {path:"date",component:DatesearchComponent},
+    {path:"month",component:MonthsearchComponent},
+    {path:"year",component:SelectwholeyearComponent}
 ];
